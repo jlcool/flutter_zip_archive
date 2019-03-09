d64fd1049f47874f259101082e16553bbeea7cad
jiangliang <jiangliang.net@gmail.com>
2019-03-09 14:02:31 +0800

    1

--- android/src/main/java/com/jlcool/flutterziparchive/FlutterZipArchivePlugin.java
+++ android/src/main/java/com/jlcool/flutterziparchive/FlutterZipArchivePlugin.java
@@ -172,33 +172,10 @@ public class FlutterZipArchivePlugin implements MethodCallHandler {
         return zip(src, null, passwd);
     }
 
-    /**
-     * 使用给定密码压缩指定文件或文件夹到当前目录
-     *
-     * @param src    要压缩的文件
-     * @param dest   压缩文件存放路径
-     * @param passwd 压缩使用的密码
-     * @return 最终的压缩文件存放的绝对路径, 如果为null则说明压缩失败.
-     */
-    public String zip(String src, String dest, String passwd) {
-        return zip(src, dest, true, passwd);
-    }
 
-    /**
-     * 使用给定密码压缩指定文件或文件夹到指定位置.
-     * <p>
-     * dest可传最终压缩文件存放的绝对路径,也可以传存放目录,也可以传null或者"".<br />
-     * 如果传null或者""则将压缩文件存放在当前目录,即跟源文件同目录,压缩文件名取源文件名,以.zip为后缀;<br />
-     * 如果以路径分隔符(File.separator)结尾,则视为目录,压缩文件名取源文件名,以.zip为后缀,否则视为文件名.
-     *
-     * @param src         要压缩的文件或文件夹路径
-     * @param dest        压缩文件存放路径
-     * @param isCreateDir 是否在压缩文件里创建目录,仅在压缩文件为目录时有效.<br />
-     *                    如果为false,将直接压缩目录下文件到压缩文件.
-     * @param passwd      压缩使用的密码
-     * @return 最终的压缩文件存放的绝对路径, 如果为null则说明压缩失败.
-     */
-    public String zip(String src, String dest, boolean isCreateDir, String passwd) {
+    public String zip(String src, String dest,  String passwd) {
+
+
         File srcFile = new File(src);
         dest = buildDestinationZipFilePath(srcFile, dest);
         ZipParameters parameters = new ZipParameters();
@@ -213,14 +190,14 @@ public class FlutterZipArchivePlugin implements MethodCallHandler {
             ZipFile zipFile = new ZipFile(dest);
             if (srcFile.isDirectory()) {
                 // 如果不创建目录的话,将直接把给定目录下的文件压缩到压缩文件,即没有目录结构
-                if (!isCreateDir) {
+
                     File[] subFiles = srcFile.listFiles();
                     ArrayList<File> temp = new ArrayList<File>();
                     Collections.addAll(temp, subFiles);
                     zipFile.addFiles(temp, parameters);
                     return dest;
-                }
-                zipFile.addFolder(srcFile, parameters);
+
+
             } else {
                 zipFile.addFile(srcFile, parameters);
             }
